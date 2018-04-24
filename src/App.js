f44764ff253cf1383b2e1dda5ecbc655820a2ddc
peterlagerstrom <peter@Peters-MacBook.local>
2018-04-24 23:45:46 +0200

    Made it look as before and removed stuff that will not be used

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ul>
+        <li><a href="http://vimeo.com/user12035599/videos" title="Vimeo" className="icon-vimeo" target="_blank" onclick="_gaq.push(['_trackEvent', 'ExternalLink', 'Vimeo']);"></a></li>
+        <li><a href="http://se.linkedin.com/in/peterlagerstrom" title="LinkedIn" className="icon-linkedin" target="_blank" onclick="_gaq.push(['_trackEvent', 'ExternalLink', 'LinkedIn']);"></a></li>
+        <li><a href="https://twitter.com/peterlagerstrom" title="Twitter" className="icon-twitter" target="_blank" onclick="_gaq.push(['_trackEvent', 'ExternalLink', 'Twitter']);"></a></li>
+        <li><a href="http://instagram.com/peterlagerstrom" title="Instagram" className="icon-instagram" target="_blank" onclick="_gaq.push(['_trackEvent', 'ExternalLink', 'Instagram']);"></a></li>
+        <li><a href="https://foursquare.com/peterlagerstrom" title="Foursquare" className="icon-foursquare" target="_blank" onlick="_gaq.push(['_trackEvent', 'ExternalLink', 'Foursquare']);"></a></li>
+      </ul>
     );
   }
 }
