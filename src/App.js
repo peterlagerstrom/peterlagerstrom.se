587b44e01dd7914481625f98acc0eae43868ca6c
peterlagerstrom <peter@Peters-MacBook.local>
2018-04-29 10:12:03 +0200

    Update to the current production

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
   render() {
@@ -8,8 +9,9 @@ class App extends Component {
         <li><a href="http://se.linkedin.com/in/peterlagerstrom" title="LinkedIn" className="icon-linkedin" target="_blank" onclick="_gaq.push(['_trackEvent', 'ExternalLink', 'LinkedIn']);"></a></li>
         <li><a href="https://twitter.com/peterlagerstrom" title="Twitter" className="icon-twitter" target="_blank" onclick="_gaq.push(['_trackEvent', 'ExternalLink', 'Twitter']);"></a></li>
         <li><a href="http://instagram.com/peterlagerstrom" title="Instagram" className="icon-instagram" target="_blank" onclick="_gaq.push(['_trackEvent', 'ExternalLink', 'Instagram']);"></a></li>
-        <li><a href="https://foursquare.com/peterlagerstrom" title="Foursquare" className="icon-foursquare" target="_blank" onlick="_gaq.push(['_trackEvent', 'ExternalLink', 'Foursquare']);"></a></li>
-      </ul>
+        <li><a href="https://foursquare.com/peterlagerstrom" title="Foursquare" className="icon-foursquare" target="_blank" onclick="_gaq.push(['_trackEvent', 'ExternalLink', 'Foursquare']);"></a></li>
+        <li><a href="https://github.com/peterlagerstrom" title="Github" className="icon-github" target="_blank" onclick="_gaq.push(['_trackEvent', 'ExternalLink', 'Github']);"></a></li>
+    </ul>
     );
   }
 }
