dcda55c42935a7d19740901ba7b5034f0d8cd05e
peterlagerstrom <peter@Peters-MacBook.local>
2020-05-24 14:03:56 +0200

    SVG icons and component
    Moved into components, added svg icons, removed old

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,34 +1,82 @@
 <template>
   <Layout>
-
-
-  <ul>
-        <BlockSmall url="http://vimeo.com/user12035599/videos" title="Vimeo" />
-        <li><a href="" title="" className="icon-vimeo" target="_blank" onclick="_gaq.push(['_trackEvent', 'ExternalLink', 'Vimeo']);"></a></li>
-        <li><a href="http://se.linkedin.com/in/peterlagerstrom" title="LinkedIn" class="icon-linkedin" target="_blank" onclick="_gaq.push(['_trackEvent', 'ExternalLink', 'LinkedIn']);"></a></li>
-        <li><a href="https://twitter.com/peterlagerstrom" title="Twitter" class="icon-twitter" target="_blank" onclick="_gaq.push(['_trackEvent', 'ExternalLink', 'Twitter']);"></a></li>
-        <li><a href="http://instagram.com/peterlagerstrom" title="Instagram" class="icon-instagram" target="_blank" onclick="_gaq.push(['_trackEvent', 'ExternalLink', 'Instagram']);"></a></li>
-        <li><a href="https://foursquare.com/peterlagerstrom" title="Foursquare" class="icon-foursquare" target="_blank" onclick="_gaq.push(['_trackEvent', 'ExternalLink', 'Foursquare']);"></a></li>
-        <li><a href="https://github.com/peterlagerstrom" title="Github" class="icon-github" target="_blank" onclick="_gaq.push(['_trackEvent', 'ExternalLink', 'Github']);"></a></li>
+    <ul>
+      <BlockSmall url="http://vimeo.com/user12035599/videos" title="Vimeo" />
+      <BlockSmall url="http://se.linkedin.com/in/peterlagerstrom" title="LinkedIn" />
+      <BlockSmall url="https://twitter.com/peterlagerstrom" title="Twitter" />
+      <BlockSmall url="https://instagram.com/peterlagerstrom" title="Instagram" />  
+      <BlockSmall url="https://github.com/peterlagerstrom" title="Github" />
     </ul>   
   </Layout>
 </template>
 
 <script>
-import BlockSmall from '~/components/BlockSmall.vue'
+  import BlockSmall from '~/components/BlockSmall.vue'
 
-export default {
-  components: {
-    BlockSmall
-  },
-  metaInfo: {
-    title: ''
+  export default {
+    components: {
+      BlockSmall
+    },
+    metaInfo: {
+      title: 'Peter Lagerstr&ouml;m'
+    }
   }
-}
 </script>
 
 <style>
-.home-links a {
-  margin-right: 1rem;
+* {
+  margin:0;
+  padding:0
 }
+
+html,body{
+  margin:0;
+  width:100%;
+  height:100%;
+  background:#fff;
+  background:-moz-linear-gradient(top,#fff 0,#eaeaea 100%);
+  background:-webkit-gradient(linear,left top,left bottom,color-stop(0%,#fff),color-stop(100%,#eaeaea));
+  background:-webkit-linear-gradient(top,#fff 0,#eaeaea 100%);
+  background:-o-linear-gradient(top,#fff 0,#eaeaea 100%);
+  background:-ms-linear-gradient(top,#fff 0,#eaeaea 100%);
+  background:linear-gradient(to bottom,#fff 0,#eaeaea 100%);
+  filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#ffffff',endColorstr='#eaeaea',GradientType=0);
+  overflow:hidden
+}
+
+ul{
+  width: auto;
+  text-align: center;
+  margin: 20px auto 0;
+  font-size:5em;
+
+}
+@media only screen and (min-width: 700px) {
+ul{
+  width:700px;
+  margin:200px auto 0;
+    }
+  }
+
+a,a:hover,
+a.visited, 
+a.active{
+  text-decoration:none
+}
+
+li {
+  display: block;
+}
+@media only screen and (min-width: 700px) {
+li {
+  display: inline-block;
+}
+  }
+.icon {
+  width: 80px;
+  display:inline-block;
+  margin:0 30px
+}
+
+
 </style>
